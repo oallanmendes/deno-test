ff7b046cb0a1d0687b63cb5bf78e76c5cf570d46
Allan Mendes <oallanmendes@gmail.com>
2024-12-22 18:53:30 -0300

    change from localhost to 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ app.get('/', (request, response) => {
   return response.send("<h1>HELLO FROM NODE</h1>")
 })
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Running')
 })
